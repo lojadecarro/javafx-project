4b8fab20449592eaf9ecbb78ed982cee9240b210
Tro Armen <troarmen@gmail.com>
2023-10-25 12:47:05 -0300

    avancos novos carros

--- src/main/java/com/example/javafxproject/CadastrarNovosCarros.java
+++ src/main/java/com/example/javafxproject/CadastrarNovosCarros.java
@@ -6,16 +6,18 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ComboBox;
 
 public class CadastrarNovosCarros {
-    // @FXML
-    // private TextField txfCidade;
-    // @FXML
-    // private TextField txfEstado;
-    // @FXML
-    // private TextField txfPlaca;
-    // @FXML
-    // private TextField txfAno;
-    // @FXML
-    // private TextField txfFabricante;
+    @FXML
+    private TextField txfCidade;
+    @FXML
+    private TextField txfEstado;
+    @FXML
+    private TextField txfPlaca;
+    @FXML
+    private TextField txfAno;
+    @FXML
+    private TextField txfFabricante;
+    @FXML
+    private TextField txfCor;
     // @FXML
     // private CheckBox ckbNovo;
     // @FXML
@@ -33,17 +35,15 @@ public class CadastrarNovosCarros {
     // @FXML
     // private CheckBox ckbManual;
     // @FXML
-    // private TextField txfCor;
-    @FXML
-    private ComboBox<String> cboModelo;
+    // private ComboBox<String> cboModelo;
 
     public void onActionCadastrarcarronovo() {
-        // String cidade = txfCidade.getText();
-        // String estado = txfEstado.getText();
-        // String placa = txfPlaca.getText();
-        // String ano = txfAno.getText();
-        // String cor = txfCor.getText();
-        // String fabricante = txfFabricante.getText();
+        String cidade = txfCidade.getText();
+        String estado = txfEstado.getText();
+        String placa = txfPlaca.getText();
+        String ano = txfAno.getText();
+        String fabricante = txfFabricante.getText();
+        String cor = txfCor.getText();
         // Boolean novo = ckbNovo.isSelected();
         // Boolean seminovo = ckbSeminovo.isSelected();
         // Boolean usado = ckbUsado.isSelected();
@@ -53,25 +53,26 @@ public class CadastrarNovosCarros {
         // Boolean automatico = ckbAutomatico.isSelected();
         // Boolean manual = ckbManual.isSelected();
 
-        String modelo = cboModelo.getValue();
+        // String modelo = cboModelo.getValue();
 
 
-        // System.out.println(cidade);
-        // System.out.println(estado);
-        // System.out.println(placa);
-        // System.out.println(ano); 
-        // System.out.println(fabricante);
+        System.out.println(cidade);
+        System.out.println(estado);
+        System.out.println(placa);
+        System.out.println(ano); 
+        System.out.println(fabricante);
+        System.out.println(cor);
         // System.out.println(novo);
         // System.out.println(seminovo);
         // System.out.println(usado); 
         // System.out.println(sedan);
         // System.out.println(hatch);
         // System.out.println(suv);         
-        // System.out.println(cor);
+        
         // System.out.println(automatico);
         // System.out.println(manual);
 
-        System.out.println(modelo);
+        // System.out.println(modelo);
 
 
 
